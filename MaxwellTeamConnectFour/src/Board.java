8819b2994ce3a12976afa11045899f20c43ba971
323963082 <323963082@C-8NTT8V1.tdsb.on.ca>
2018-04-11 14:27:22 -0400

    added gamepice, Colour and board

---
+++ MaxwellTeamConnectFour/src/Board.java
@@ -0,0 +1,10 @@
+
+public class Board {
+
+	private GamePiece[][] board= new GamePiece[6][7];	
+	
+	public void changeBoard(int row, int col, Colour colour){
+		board[row][col] = new GamePiece(colour);
+	}
+	
+}
---
+++ MaxwellTeamConnectFour/src/GamePiece.java
@@ -0,0 +1,10 @@
+
+public class GamePiece {
+	//0 is blue, 1 is red
+	private Colour pieceColour;
+	
+	public GamePiece(Colour col){
+		pieceColour = col;
+	}
+}
+
