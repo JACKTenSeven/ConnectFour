5f7fd01400b888ee42e88167354da1f5caacf2a0
323963082 <323963082@C-8NTT8V1.tdsb.on.ca>
2018-04-13 12:57:51 -0400

    fffixed piece falling, i think

--- MaxwellTeamConnectFour/src/Board.java
+++ MaxwellTeamConnectFour/src/Board.java
@@ -3,6 +3,11 @@ public class Board {
 
 	public GamePiece[][] board= new GamePiece[6][7];	
 	
+	/**
+	 * places piece of the desired colour at the top of the column desired
+	 * @param col	the column to place the piece
+	 * @param colour	colour of the piece
+	 */
 	public void placePiece(int col, Colour colour){
 		board[0][col] = new GamePiece(colour);
 		printBoard();
@@ -11,10 +16,15 @@ public class Board {
 		board[0][col] = null;
 		printBoard();
 	}
-	
+	/**
+	 * Makes the piece at the top of the column go
+	 * to the lowest possible position on the board
+	 * @param col	The column where the just placed piece is
+	 * @param colour	the colour of the piece, red or blue
+	 */
 	private void pieceFall(int col, Colour colour){
 		for(int i = 1; i < board.length; i++){
-			if(i == board.length-1){
+			if(i == board.length-1 && board[i][board.length-1] == null){
 				board[i][col] = new GamePiece(colour);
 			}
 			else if(board[i][col] == null){
