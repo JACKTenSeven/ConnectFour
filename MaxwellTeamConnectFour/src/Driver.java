6ef2d66fad9cb657f8dc198ea7eb6b715bcf8ce8
Jack Maxwell <323950766@C-MXL3150J9T.tdsb.on.ca>
2018-04-13 14:04:25 -0400

    Delete old file drivr interactability

--- MaxwellTeamConnectFour/src/Driver.java
+++ MaxwellTeamConnectFour/src/Driver.java
@@ -1,12 +1,49 @@
-
-
-
+import java.util.Scanner;
 
 public class Driver  {
-
-	
-	void placePiece(float col){
+	public static Board gameBoard = new Board();
+	public static void main(String[]args){
+		Scanner k = new Scanner(System.in);
+		boolean gameEnd=false;
+		Colour player1;
+		Colour player2;
 		
+		
+		player1 = Colour.Blue;
+		player2 = Colour.Red;
+		
+		while (!gameEnd){
+		System.out.println("Player 1 (Blue) Turn");
+		System.out.println("Enter the number key from 1-7 of the column you wish to place a game piece");
+		int loc = k.nextInt();
+		if(loc>7){
+			System.out.println("Invalid entry");
+			System.out.println("Turn forfiet");
+		}
+		else{
+		playerDrop(k.nextInt()-1,player1);
+		}
+		
+		
+		System.out.println("Player 2 (Red) Turn");
+		System.out.println("Enter the number key of the column you wish to place a game piece");
+		 loc = k.nextInt();
+		if(loc>7){
+			System.out.println("Invalid entry");
+			System.out.println("Turn forfiet");
+		}
+		else{
+		playerDrop(k.nextInt()-1,player1);
+		}
+	
+		
+		}
+		
+		
+	}
+	
+	  static void playerDrop(int col, Colour playerCol){
+		gameBoard.placePiece(col, playerCol);
 	}
 
 }
