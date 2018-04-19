25a20a106d9778cd460e8a5974e6da6c8d7e49b3
Jack Maxwell <323950766@C-MXL3150J9T.tdsb.on.ca>
2018-04-19 13:36:17 -0400

    Began work on cleaning code

--- MaxwellTeamConnectFour/src/Driver.java
+++ MaxwellTeamConnectFour/src/Driver.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 
 public class Driver {
 	public static Board gameBoard = new Board(6, 7);
-
+	public static Colour player1;
+	public static Colour player2;
+	public static Scanner k = new Scanner(System.in);
+	public static int loc;
 	public static void main(String[] args) {
-		Scanner k = new Scanner(System.in);
+
 		Random r = new Random();
 		boolean gameEnd = false;
 		boolean local = true;
-		Colour player1;
-		Colour player2;
-		
+
 		player1 = Colour.Blue;
 		player2 = Colour.Red;
 
@@ -21,15 +22,6 @@ public class Driver {
 
 		if (selection == 1) {
 			while (!gameEnd) {
-				System.out.println("Player 1 (Blue) Turn");
-				System.out.println("Enter the number key from 1-7 of the column you wish to place a game piece");
-				int loc = k.nextInt();
-				if (loc > 7) {
-					System.out.println("Invalid entry");
-					System.out.println("Turn forfiet");
-				} else {
-					playerDrop(loc - 1, player1);
-				}
 
 				System.out.println("Player 2 (Red) Turn");
 				System.out.println("Enter the number key of the column you wish to place a game piece");
@@ -43,7 +35,7 @@ public class Driver {
 
 			}
 		}
-		
+
 		if (selection == 2) {
 			while (!gameEnd) {
 				System.out.println("Player 1 (Blue) Turn");
@@ -57,14 +49,21 @@ public class Driver {
 				}
 
 				System.out.println("Computer (Red) Turn");
-				loc = r.nextInt(7-1)+1;
+				loc = r.nextInt(7 - 1) + 1;
 				playerDrop(loc - 1, player2);
-				
 
 			}
-		
+
 		}
 
+	}
+
+	static void player1Turn() {
+		System.out.println("Player 1 (Blue) Turn");
+		System.out.println("Enter the number key from 1-7 of the column you wish to place a game piece");
+		loc = k.nextInt();
+		gameBoard.colFull(loc-1);
+		playerDrop(loc - 1, player1);
 	}
 
 	static void playerDrop(int col, Colour playerCol) {
