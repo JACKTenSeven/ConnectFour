5bee23a7a47fe3f16000c3219a03041509dadf86
Jack Maxwell <323950766@C-MXL3150J9T.tdsb.on.ca>
2018-04-19 13:14:48 -0400

    PEASE I JUST WANT TO DIE

--- MaxwellTeamConnectFour/src/Driver.java
+++ MaxwellTeamConnectFour/src/Driver.java
@@ -1,50 +1,74 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Driver  {
-	public static Board gameBoard = new Board(6,7);
-	public static void main(String[]args){
+public class Driver {
+	public static Board gameBoard = new Board(6, 7);
+
+	public static void main(String[] args) {
 		Scanner k = new Scanner(System.in);
-		boolean gameEnd=false;
+		Random r = new Random();
+		boolean gameEnd = false;
+		boolean local = true;
 		Colour player1;
 		Colour player2;
-		//GOOD JOB
 		
 		player1 = Colour.Blue;
 		player2 = Colour.Red;
-		
-		while (!gameEnd){
-		System.out.println("Player 1 (Blue) Turn");
-		System.out.println("Enter the number key from 1-7 of the column you wish to place a game piece");
-		int loc = k.nextInt();
-		if(loc>7){
-			System.out.println("Invalid entry");
-			System.out.println("Turn forfiet");
+
+		System.out.println("Enter 1 to play with a friend");
+		System.out.println("Enter 2 to play against the computer");
+		int selection = k.nextInt();
+
+		if (selection == 1) {
+			while (!gameEnd) {
+				System.out.println("Player 1 (Blue) Turn");
+				System.out.println("Enter the number key from 1-7 of the column you wish to place a game piece");
+				int loc = k.nextInt();
+				if (loc > 7) {
+					System.out.println("Invalid entry");
+					System.out.println("Turn forfiet");
+				} else {
+					playerDrop(loc - 1, player1);
+				}
+
+				System.out.println("Player 2 (Red) Turn");
+				System.out.println("Enter the number key of the column you wish to place a game piece");
+				loc = k.nextInt();
+				if (loc > 7) {
+					System.out.println("Invalid entry");
+					System.out.println("Turn forfiet");
+				} else {
+					playerDrop(loc - 1, player1);
+				}
+
+			}
 		}
-		else{
-		playerDrop(loc-1,player1);
+		
+		if (selection == 2) {
+			while (!gameEnd) {
+				System.out.println("Player 1 (Blue) Turn");
+				System.out.println("Enter the number key from 1-7 of the column you wish to place a game piece");
+				int loc = k.nextInt();
+				if (loc > 7) {
+					System.out.println("Invalid entry");
+					System.out.println("Turn forfiet");
+				} else {
+					playerDrop(loc - 1, player1);
+				}
+
+				System.out.println("Computer (Red) Turn");
+				loc = r.nextInt(7-1)+1;
+				playerDrop(loc - 1, player1);
+				
+
+			}
+		
 		}
-		
-		
-		System.out.println("Player 2 (Red) Turn");
-		System.out.println("Enter the number key of the column you wish to place a game piece");
-		 loc = k.nextInt();
-		if(loc>7){
-			System.out.println("Invalid entry");
-			System.out.println("Turn forfiet");
-		}
-		else{
-		playerDrop(loc-1,player2);
-		}
-	
-		
-		}
-		
-		
+
 	}
-	
-	  static void playerDrop(int col, Colour playerCol){
-		  System.out.println("HERE");
-		  gameBoard.placePiece(col, playerCol);
+
+	static void playerDrop(int col, Colour playerCol) {
+		gameBoard.placePiece(col, playerCol);
 	}
 
 }
