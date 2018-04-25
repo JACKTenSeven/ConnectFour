9c7908c44ad1e42bd7de9ad7c2fb9cc3ae343bf8
323963082 <323963082@C-8NTT8V1.tdsb.on.ca>
2018-04-25 13:47:01 -0400

    added grafics

---
+++ MaxwellTeamConnectFour/src/GUIDriver.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIDriver extends Application {
+	public static Board gameBoard = new Board(6, 7);
+	public static Colour player1;
+	public static Colour player2;
+	
+	
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+		
+		
+	}
+	
+	
+
+	
+}
